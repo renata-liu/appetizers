bc12d2e4186cf3b188f0884aab99591b5deff808
Renata Liu <renata_liu@outlook.com>
2025-04-19 23:22:35 -0700

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Renata Liu on 2025-04-19.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
